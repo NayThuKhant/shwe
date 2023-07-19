73bc16be995db1a55ca408379e6f8803ad131916
Nay Thu Khant <naythukhant644@gmail.com>
2023-07-19 20:04:41 +0630

    Service workder added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-    <div class="min-h-screen flex flex-col items-center py-5 bg-gray-300">
-        <h1 class="text-center font-bold text-2xl mb-5">မြန်မာ့ရွှေ</h1>
-        <RouterView/>
+  <div class="min-h-screen flex flex-col items-center py-5 bg-gray-300">
+    <h1 class="text-center font-bold text-2xl mb-5">မြန်မာ့ရွှေ</h1>
+    <RouterView />
 
-        <p class="mt-3">Crafted by <a class="text-blue-400" href="https://github.com/naythukhant">Nay Thu Khant</a></p>
-    </div>
+    <p class="mt-3">
+      Crafted by <a class="text-blue-400" href="https://github.com/naythukhant">Nay Thu Khant</a>
+    </p>
+  </div>
 </template>
--- src/components/InputGroup.vue
+++ src/components/InputGroup.vue
@@ -3,24 +3,29 @@
     <label class="block text-gray-700 text-sm font-bold mb-2" :for="id">
       {{ label }}
     </label>
-    <input class="appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-           :id="id" :type="type" :placeholder="placeholder" :disabled="disabled" :value="modelValue" @input="updateValue($event.target.value)">
+    <input
+      class="appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      :id="id"
+      :type="type"
+      :placeholder="placeholder"
+      :disabled="disabled"
+      :value="modelValue"
+      @input="updateValue($event.target.value)"
+    />
   </div>
 </template>
 
 <script setup>
-import { ref, defineProps, defineEmits } from 'vue';
+import { ref, defineProps, defineEmits } from 'vue'
 
-const props = defineProps(['model', 'label', 'type', 'placeholder', 'id', 'disabled']);
-const { emit } = defineEmits(['update:model']);
-const modelValue = ref(props.model);
+const props = defineProps(['model', 'label', 'type', 'placeholder', 'id', 'disabled'])
+const { emit } = defineEmits(['update:model'])
+const modelValue = ref(props.model)
 
 const updateValue = (value) => {
-  modelValue.value = value;
-  emit('update:model', value);
-};
+  modelValue.value = value
+  emit('update:model', value)
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
