6085781255b0fb1ba6c0523ad98154c0ef50d977
Nay Thu Khant <naythukhant644@gmail.com>
2023-07-19 16:28:23 +0630

    Initial release

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 
 <template>
     <div class="min-h-screen flex flex-col items-center py-5 bg-gray-300">
-        <h1 class="text-center font-bold text-2xl mb-5">ရွှေ</h1>
+        <h1 class="text-center font-bold text-2xl mb-5">မြန်မာ့ရွှေ</h1>
         <RouterView/>
     </div>
 </template>
