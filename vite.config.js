1f2315f0f9b57ac01311052a61a1a7329af2a9c5
Nay Thu Khant <naythukhant644@gmail.com>
2023-07-19 20:01:31 +0630

    Service workder added

--- vite.config.js
+++ vite.config.js
@@ -10,14 +10,30 @@ export default defineConfig({
         vue(),
         VitePWA({
             registerType: "autoUpdate",
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg}']
+            },
             manifest: {
                 name: "မြန်မာ့ရွှေ",
                 short_name: "မြန်မာ့ရွှေ",
+                description: "မြန်မာပြည်တွင် ဝယ်ယူ/ရောင်းချ လိုသောရွှေ ကို ဈေးနှုန်းတွက်ချက်နိုင်မည့် အက်ပလီကေးရှင်း",
                 icons: [
                     {
-                        src: "/icons/512.jpeg",
+                        src: "/icons/pwa-64x64.png",
+                        sizes: "64x64",
+                        type: "image/png",
+                        purpose: "any maskable"
+                    },
+                    {
+                        src: "/icons/pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "any maskable"
+                    },
+                    {
+                        src: "/icons/pwa-512x512.png",
                         sizes: "512x512",
-                        type: "image/jpeg",
+                        type: "image/png",
                         purpose: "any maskable"
                     }
                 ]
