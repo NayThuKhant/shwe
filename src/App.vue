e6eb90983474a2acb807602422c3147559d3454e
Nay Thu Khant <naythukhant644@gmail.com>
2023-07-19 17:10:45 +0630

    Initial release

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,7 @@
     <div class="min-h-screen flex flex-col items-center py-5 bg-gray-300">
         <h1 class="text-center font-bold text-2xl mb-5">မြန်မာ့ရွှေ</h1>
         <RouterView/>
+
+        <p class="mt-3">Crafted by <a class="text-blue-400" href="https://github/naythukhant">Nay Thu Khant</a></p>
     </div>
 </template>
